9b538545328dd22baad6f691175f288f4412dcdd
unisystechhub <ramesh.55555.k@example.com>
2025-03-14 09:17:51 +0530

    fix - helath check faild when redis is not up

--- TransactionService/src/main/java/com/example/demo/TransactionServiceApplication.java
+++ TransactionService/src/main/java/com/example/demo/TransactionServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class
+	})
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {

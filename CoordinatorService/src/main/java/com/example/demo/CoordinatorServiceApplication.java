fb69d8cd95b57b3c32c524a271420c0f3224a017
unisystechhub <ramesh.55555.k@example.com>
2025-03-16 10:10:32 +0530

    delete Redis,jap depenencies -coordinator service

--- CoordinatorService/src/main/java/com/example/demo/CoordinatorServiceApplication.java
+++ CoordinatorService/src/main/java/com/example/demo/CoordinatorServiceApplication.java
@@ -3,9 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-	    org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class
-	})
+@SpringBootApplication
 public class CoordinatorServiceApplication {
 
 	public static void main(String[] args) {

a8bb4a5051f371cd358312c34b2a32aa906d8c90
unisystechhub <ramesh.55555.k@example.com>
2025-05-23 17:19:25 +0530

    allow localhost 4200

--- UserService/src/main/java/com/banking/auth/security/WebSecurityConfig.java
+++ UserService/src/main/java/com/banking/auth/security/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // React app origin
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:4200")); // React app origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Only needed if using cookies
